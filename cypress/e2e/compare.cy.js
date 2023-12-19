77b7ae7bce722aa32c6c728e0bc0b3de1bb7134d
JRafaCand <r.candilejo@gmail.com>
2023-12-19 15:25:15 -0500

    Prueba

--- cypress/e2e/compare.cy.js
+++ cypress/e2e/compare.cy.js
@@ -1,4 +1,4 @@
-import { validarTitulos } from "../support/utils";
+import { validarContenido, validarTitulos } from "../support/utils";
 
 
 describe('compare', () => {
@@ -6,10 +6,21 @@ describe('compare', () => {
     cy.visit('https://antarcticbookingcenter.com/ecommerce/antarctic/inquire/MTAy/1145');
     cy.get('title').invoke('text').as('titlePrimary');
     cy.wait(1000);
-    cy.visit('https://antarcticbookingcenter.com/ecommerce-develop/antarctic/search/0/0/1/2024/0/90000/0/0/0/0/4/0/0');
+    cy.get('h4').invoke('text').as('h4text');
+    cy.wait(1000);
+    cy.get('.details > .col-lg-7').invoke('text').as('textcolrig');
+    cy.visit('https://antarcticbookingcenter.com/ecommerce-develop/antarctic/inquire/MTAy/1145');
     cy.get('title').invoke('text').as('titleSecond');
     cy.wait(1000);
     validarTitulos('@titlePrimary', '@titleSecond');
+    cy.wait(1000);
+    cy.get('h4').invoke('text').as('h4text1');
+    cy.wait(1000);
+    validarContenido('@h4text', '@h4text1');
+    cy.wait(1000);
+    cy.get('.details > .col-lg-7').invoke('text').as('textcolrig1');
+    cy.wait(1000);
+    validarContenido('@textcolrig', '@textcolrig1');
     
   });
   
